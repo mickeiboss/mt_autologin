97b3c4d19f62ea9174478eff8e9b9a2012bd194c
zlgry112 <zlgry112@DESKTOP-E56P7L9>
2025-09-11 18:49:34 +0800

    optimized the code

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# mt_autologin
+# **mt_autologin**
+
+推荐部署在青龙面板上使用
+
+需要科学上网
+
+## 下载脚本
+
+下载mt_autologin.py并上传到青龙面板
+
+## 安装依赖
+
+安装python依赖: selenium, pyotp
+
+安装linux依赖: chromium, chromium-chromedriver
+
+## 设置环境变量
+
+USERNAME = your_username
+
+PASSWORD = your_password
+
+SECRET_KEY = your_secret_key
+
+DRIVER_PATH = your_driver_path
+
+## 设置定时任务
--- mt_autologin.py
+++ mt_autologin.py
@@ -4,75 +4,70 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-import time
+# import time
 import pyotp
 import os
 
 # selenium模拟登录
-def simulate_login(username, password, secret_key):
+def simulate_login(username, password, secret_key, driver_path):
 
     # 配置options
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    browser_options = Options()
+    browser_options.add_argument('--headless')
+    browser_options.add_argument('--no-sandbox')
+    browser_options.add_argument('--disable-gpu')
+    browser_options.add_argument('--disable-dev-shm-usage')
 
     # 创建service
-    chrome_service = Service(executable_path =r'.\chromedriver.exe')
+    browser_service = Service(executable_path = driver_path)
 
     # 初始化webdriver
-    chrome_driver = webdriver.Chrome(service = chrome_service, options = chrome_options)
+    browser_driver = webdriver.Chrome(service = browser_service, options = browser_options)
 
     # 访问网页
-    chrome_driver.get('https://ob.m-team.cc')
+    browser_driver.get('https://ob.m-team.cc')
 
     # 获取网页标题
-    print('网页标题:', chrome_driver.title)
-
-    # 等待加载
-    login_element_01 = WebDriverWait(chrome_driver, 10).until(
-        expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='登 錄']"))
-    )
+    print('网页标题:', browser_driver.title)
 
     # 输入用户名
-    time.sleep(1)
-    username_element = chrome_driver.find_element(By.ID, 'username')
+    username_element = WebDriverWait(browser_driver, 10).until(
+        expected_conditions.visibility_of_element_located((By.ID, 'username'))
+    )
     username_element.send_keys(username)
 
     # 输入密码
-    time.sleep(1)
-    password_element = chrome_driver.find_element(By.ID, 'password')
+    password_element = WebDriverWait(browser_driver, 10).until(
+        expected_conditions.visibility_of_element_located((By.ID, 'password'))
+    )
     password_element.send_keys(password)
 
     # 登录
-    time.sleep(1)
+    login_element_01 = WebDriverWait(browser_driver, 10).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='登 錄']"))
+    )
     login_element_01.click()
 
     try:
-
-        # 等待加载
-        totp_code_element = WebDriverWait(chrome_driver, 10).until(
-            expected_conditions.visibility_of_element_located((By.ID, 'totpcode'))
-        )
-
         # 输入验证码
-        time.sleep(1)
         totp_code = pyotp.TOTP(secret_key).now()
+        totp_code_element = WebDriverWait(browser_driver, 10).until(
+            expected_conditions.visibility_of_element_located((By.ID, 'otpCode'))
+        )
         totp_code_element.send_keys(totp_code)
 
         # 登录
-        time.sleep(1)
-        login_element_02 = chrome_driver.find_element(By.XPATH, "//span[text()='登 錄']")
+        login_element_02 = WebDriverWait(browser_driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='登 錄']"))
+        )
         login_element_02.click()
 
     except:
         print('未发现验证')
 
     try:
-
         # 确认通知
-        confirm_element = WebDriverWait(chrome_driver, 10).until(
+        confirm_element = WebDriverWait(browser_driver, 10).until(
             expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='確認']"))
         )
         confirm_element.click()
@@ -81,18 +76,22 @@ def simulate_login(username, password, secret_key):
         print('未发现通知')
 
     # 关闭浏览器
+    # user_input = input('输入q关闭浏览器:')
+    # if user_input == 'q':
+        browser_driver.quit()
 
-    if __name__ == "__main__":
-        print('登陆完成')
-        user_input = input('输入q关闭浏览器:')
-        if user_input == 'q':
-            chrome_driver.quit()
+# 定义主函数
+def main():
 
+    # 获取环境变量
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    secret_key = os.getenv('SECRET_KEY')
+    driver_path = os.getenv('DRIVER_PATH')
 
-# 获取环境变量
-USERNAME = os.getenv('username')
-PASSWORD = os.getenv('password')
-SECRET_KEY = os.getenv('secret_key')
+    # 登录
+    simulate_login(username, password, secret_key, driver_path)
 
-# 登录
-simulate_login(USERNAME, PASSWORD, SECRET_KEY)
+if __name__ == '__main__':
+    main()
+    print('登陆成功')
