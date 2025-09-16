61b66421e74b429c80686c473e8c5104afffe5b8
zlgry112 <zlgry112@DESKTOP-E56P7L9>
2025-09-16 17:51:44 +0800

    add: add login check refactor: POP -> OOP docs: update README.md

--- README.md
+++ README.md
@@ -1,20 +1,40 @@
 # **mt_autologin**
 
-推荐部署在青龙面板上使用
+使用 selenium 创建无头浏览器，通过模拟操作登录~~或通过登录凭证登录~~
 
-需要科学上网
+支持通过[青龙面板]([Qinglong](https://qinglong.online/))或~~docker部署（开发中）~~
 
-## 下载脚本
+> 注意：需要科学上网
 
-下载mt_autologin.py并上传到青龙面板
+> 注意：仅建议将此脚本作为备用
 
-## 安装依赖
+> 注意：通过脚本登陆时会产生登录通知
+
+> ~~注意：通过登录凭证登录时可能会产生“疑似登录凭证泄露”的通知~~
+
+## 1. 通过青龙面板部署
+
+### 1.1 下载脚本
+
+下载mt_autologin.py
+
+新建mt_autologin文件夹
+
+将mt_autologin.py上传至mt_autologin
+
+### 1.2 修改镜像源（可选）
+
+Python软件包镜像源：https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+
+Linux软件包镜像源：https://mirrors.tuna.tsinghua.edu.cn
+
+### 1.3 安装依赖
 
 安装python依赖: selenium, pyotp
 
 安装linux依赖: chromium, chromium-chromedriver
 
-## 设置环境变量
+### 1.4 设置环境变量
 
 USERNAME = your_username
 
@@ -24,4 +44,6 @@ SECRET_KEY = your_secret_key
 
 DRIVER_PATH = your_driver_path
 
-## 设置定时任务
+### 1.5 设置定时任务
+
+## ~~2. 通过docker部署（开发中）~~
--- mt_autologin.py
+++ mt_autologin.py
@@ -4,94 +4,237 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-# import time
+from selenium.common import TimeoutException
 import pyotp
 import os
+# import json
 
-# selenium模拟登录
-def simulate_login(username, password, secret_key, driver_path):
+# 定义AutoLogin类
+class AutoLogin:
 
-    # 配置options
-    browser_options = Options()
-    browser_options.add_argument('--headless')
-    browser_options.add_argument('--no-sandbox')
-    browser_options.add_argument('--disable-gpu')
-    browser_options.add_argument('--disable-dev-shm-usage')
+    def __init__(self, driver_path: str, disable_headless: bool):
 
-    # 创建service
-    browser_service = Service(executable_path = driver_path)
+        # 配置options
+        browser_options = Options()
 
-    # 初始化webdriver
-    browser_driver = webdriver.Chrome(service = browser_service, options = browser_options)
+        if not disable_headless:
+            browser_options.add_argument('--headless')
 
-    # 访问网页
-    browser_driver.get('https://ob.m-team.cc')
+        browser_options.add_argument('--no-sandbox')
+        browser_options.add_argument('--disable-gpu')
+        browser_options.add_argument('--disable-dev-shm-usage')
 
-    # 获取网页标题
-    print('网页标题:', browser_driver.title)
+        # 创建service
+        browser_service = Service(executable_path = driver_path)
 
-    # 输入用户名
-    username_element = WebDriverWait(browser_driver, 10).until(
-        expected_conditions.visibility_of_element_located((By.ID, 'username'))
-    )
-    username_element.send_keys(username)
+        # 初始化webdriver
+        self.browser_driver = webdriver.Chrome(service = browser_service, options = browser_options)
 
-    # 输入密码
-    password_element = WebDriverWait(browser_driver, 10).until(
-        expected_conditions.visibility_of_element_located((By.ID, 'password'))
-    )
-    password_element.send_keys(password)
+    # 等待可见元素
+    def wait_visible_elements(self, timeout: int, element: tuple[str,str]):
+        try:
+            visible_elements = WebDriverWait(self.browser_driver, timeout).until(
+                expected_conditions.visibility_of_element_located(element)
+            )
+            return visible_elements
 
-    # 登录
-    login_element_01 = WebDriverWait(browser_driver, 10).until(
-        expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='登 錄']"))
-    )
-    login_element_01.click()
+        except TimeoutException:
+            # print('未找到可见元素')
+            return False
 
-    try:
-        # 输入验证码
-        totp_code = pyotp.TOTP(secret_key).now()
-        totp_code_element = WebDriverWait(browser_driver, 10).until(
-            expected_conditions.visibility_of_element_located((By.ID, 'otpCode'))
-        )
-        totp_code_element.send_keys(totp_code)
+    # 等待可点击元素
+    def wait_clickable_elements(self, timeout: int, element: tuple[str,str]):
+        try:
+            clickable_elements = WebDriverWait(self.browser_driver, timeout).until(
+                expected_conditions.element_to_be_clickable(element)
+            )
+            return clickable_elements
+
+        except TimeoutException:
+            # print('未找到可点击元素')
+            return False
+
+    # 获取localstorage
+    # def get_local_storage(self):
+    #
+    #     # 初始化字典
+    #     local_storage = {}
+    #
+    #     # 获取localstorage中的值
+    #     local_storage_keys = ('auth', 'persist:user')
+    #     for key in local_storage_keys:
+    #         value = self.browser_driver.execute_script(f"return localStorage.getItem('{key}');")
+    #
+    #         # 解析json
+    #         try:
+    #             local_storage[key] = json.loads(value)
+    #
+    #         except json.JSONDecodeError:
+    #             local_storage[key] = value
+    #
+    #     # 保存json文件
+    #     with open('localstorage.json', 'w', encoding='utf-8') as f:
+    #         json.dump(local_storage, f, ensure_ascii=False, indent=4)
+
+    # 登陆验证
+    def check(self) -> bool:
+
+        check_condition_1 = self.wait_visible_elements(10, (By.XPATH, "//span[text()='歡迎回來']"))
+        check_condition_2 = self.wait_clickable_elements(10, (By.XPATH, "//span[text()='退出']"))
+
+        try:
+            # 确认通知
+            confirm_element = self.wait_clickable_elements(10, (By.XPATH, "//span[text()='確認']"))
+            confirm_element.click()
+            return True
+
+        except Exception:
+            if check_condition_1 or check_condition_2:
+                print('未发现通知')
+                return True
+            else:
+                print('登陆超时')
+                return False
+
+    # 模拟登录
+    def by_simulation(self, username: str, password: str, secret_key: str):
+
+        # 输入用户名
+        username_element = self.wait_visible_elements(10, (By.ID, 'username'))
+        username_element.send_keys(username)
+
+        # 输入密码
+        password_element = self.wait_visible_elements(10, (By.ID, 'password'))
+        password_element.send_keys(password)
 
         # 登录
-        login_element_02 = WebDriverWait(browser_driver, 10).until(
-            expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='登 錄']"))
-        )
-        login_element_02.click()
+        login_element_01 = self.wait_visible_elements(10, (By.XPATH, "//span[text()='登 錄']"))
+        login_element_01.click()
 
-    except:
-        print('未发现验证')
+        try:
+            # 输入验证码
+            totp_code_element = self.wait_visible_elements(10, (By.ID, 'otpCode'))
+            totp_code = pyotp.TOTP(secret_key).now()
+            totp_code_element.send_keys(totp_code)
 
-    try:
-        # 确认通知
-        confirm_element = WebDriverWait(browser_driver, 10).until(
-            expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='確認']"))
-        )
-        confirm_element.click()
+            # 登录
+            login_element_02 = self.wait_clickable_elements(10, (By.XPATH, "//span[text()='登 錄']"))
+            login_element_02.click()
 
-    except:
-        print('未发现通知')
+        except Exception:
+            if self.wait_clickable_elements(10, (By.XPATH, "//button[@type='button']")):
+                print('未发现2fa验证')
+            else:
+                print('登录超时')
+
+    # 本地存储登录
+    # def by_local_storage(self):
+    #
+    #     # 等待加载
+    #     self.wait_visible_elements(10, (By.XPATH, "//span[text()='登 錄']"))
+    #
+    #     # 清除localstorage
+    #     self.browser_driver.execute_script("localStorage.clear();")
+    #
+    #     # 注入localstorage
+    #     with open('localstorage.json', 'r', encoding='utf-8') as f:
+    #         localstorage = json.load(f)
+    #
+    #     for key, value in localstorage.items():
+    #         try:
+    #             value = json.loads(value)
+    #
+    #         except TypeError:
+    #             value = json.dumps(value, ensure_ascii=False)
+    #         except json.JSONDecodeError:
+    #             pass
+    #
+    #         self.browser_driver.execute_script(
+    #             'localStorage.setItem(arguments[0], arguments[1]);', key, value
+    #         )
+    #     # 刷新
+    #     self.browser_driver.refresh()
 
     # 关闭浏览器
-    # user_input = input('输入q关闭浏览器:')
-    # if user_input == 'q':
-        browser_driver.quit()
+    def quit(self, test: bool):
+        if test:
+            if input('输入q退出:') == 'q':
+                self.browser_driver.quit()
+        else:
+            self.browser_driver.quit()
 
 # 定义主函数
-def main():
+def main(test: bool = False, disable_headless: bool = False):
 
     # 获取环境变量
-    username = os.getenv('USERNAME')
-    password = os.getenv('PASSWORD')
-    secret_key = os.getenv('SECRET_KEY')
-    driver_path = os.getenv('DRIVER_PATH')
+    if test:
+        from tests.config import username, password, secret_key, driver_path
+    else:
+        username = os.getenv('USERNAME')
+        password = os.getenv('PASSWORD')
+        secret_key = os.getenv('SECRET_KEY')
+        driver_path = os.getenv('DRIVER_PATH')
+
+    # 初始化webdriver
+    login = AutoLogin(driver_path, disable_headless)
+
+    # 访问网页
+    login.browser_driver.get('https://ob.m-team.cc')
+
+    # 获取网页标题
+    print('网页标题:', login.browser_driver.title)
 
     # 登录
-    simulate_login(username, password, secret_key, driver_path)
+    # '''
+    # localstorage.json是否存在？
+    # │
+    # ├─ 是 → 本地存储登录 → 登陆检测是否通过？
+    # │                       │
+    # │                       ├─ 是 → 更新localstorage.json → 退出
+    # │                       │
+    # │                       └─ 否 → 模拟登录 → 登陆检测是否通过？
+    # │                                           │
+    # │                                           ├─ 是 → 更新localstorage.json → 退出
+    # │                                           │
+    # │                                           └─ 否 → 退出
+    # │
+    # └─ 否 → 模拟登录 → 登陆检测是否通过？
+    #                     │
+    #                     ├─ 是 → 更新localstorage.json → 退出
+    #                     │
+    #                     └─ 否 → 退出
+    # '''
+    # if os.path.isfile('./localstorage.json'):
+    #     login.by_local_storage()
+    #     if login.login_check():
+    #         login.get_local_storage()
+    #         login.quit_browser(test = True)
+    #         print('本地存储登陆成功')
+    #     else:
+    #         login.by_simulation(username, password, secret_key)
+    #         if login.login_check():
+    #             login.get_local_storage()
+    #             login.quit_browser(test)
+    #             print('模拟登录成功')
+    #         else:
+    #             login.quit_browser(test)
+    # else:
+    #     login.by_simulation(username, password, secret_key)
+    #     if login.login_check():
+    #         login.get_local_storage()
+    #         login.quit_browser(test)
+    #         print('模拟登录成功')
+    #     else:
+    #         login.quit_browser(test)
+
+    # 登录
+    login.by_simulation(username, password, secret_key)
+    if login.check():
+        print('模拟登录成功')
+        login.quit(test)
+    else:
+        print('模拟登录失败')
+        login.quit(test)
 
 if __name__ == '__main__':
-    main()
-    print('登陆成功')
+    main(test = False, disable_headless = False)
